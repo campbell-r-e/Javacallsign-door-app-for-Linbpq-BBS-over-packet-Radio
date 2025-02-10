d12b92e6153ab48228bdb9bbe7c2cc47a2fce77e
bsucs <122378640+bsucs@users.noreply.github.com>
2025-02-10 17:32:23 -0500

    updated

--- src/main/java/kh/hamqthclient/HamQTHClientPropertiesNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package kh.hamqthclient;
-
-public class HamQTHClientPropertiesNotFoundException extends RuntimeException {
-
-	public HamQTHClientPropertiesNotFoundException(String msg, Throwable t) {
-		super(msg, t);
-	}
-	
-}
